10cfc54bbf42e08ac19d89f2e5f9f25a2db022d7
Preet Kamboj <preetkamboj7138@gmail.com>
2026-04-22 14:48:45 +0530

    Edited index file

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,9 @@ function App() {
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
+          <p>
+            Edited index file
+          </p>
         </div>
         <button
           className="counter"
